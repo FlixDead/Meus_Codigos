c895e39d23cc32efd6713cdf76df90f20ddf6ba7
FlixDead <jeanmarcus52@gmail.com>
2025-05-18 20:31:02 -0300

    calculadora pog

--- calculadora.c
+++ calculadora.c
@@ -8,7 +8,7 @@ int main()
     scanf("%d", &num1);
     printf("EScolha outro numero: ");
     scanf("%d", &num2);
-    printf("Qual operassaum voce quer fazer?\n1 - Soma\n2 - Subtrassaum\n3 - Multiplicassaum\n4 - Divizaum\n");
+    printf("Qual operacao voce quer fazer?\n1 - Soma\n2 - Subtracao\n3 - Multiplicacao\n4 - Divisao\n");
     scanf("%d", &umaquatro);
     switch (umaquatro){
         case 1:{
@@ -16,16 +16,16 @@ int main()
             break;
         }
         case 2:{
-            printf("A subtrassaum de %d e %d e igual a: %d", num1, num2, num1 - num2);
+            printf("A subtracao de %d e %d e igual a: %d", num1, num2, num1 - num2);
             break;
         }
         case 3:{
-            printf("A Multiplicassaum de %d e %d e igual a: %d", num1, num2, num1 * num2);
+            printf("A Multiplicacao de %d e %d e igual a: %d", num1, num2, num1 * num2);
             break;
         }
         case 4:{
             umaquatro2 = (double)num1 / num2;
-            printf("A Divissaum de %d e %d e igual a: %0.2f", num1, num2, umaquatro2);
+            printf("A Divisao de %d e %d e igual a: %0.2f", num1, num2, umaquatro2);
             break;
         }
     }
